0eaa524f485d437397bc6c8c6fd59e54a5becb08
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2021-03-18 10:39:59 +0100

    Indent selection 2 and 4 spaces

---
+++ src/IndentSelector.js
@@ -0,0 +1,18 @@
+import React                from 'react';
+import { TriangleDownIcon } from '@primer/octicons-react';
+
+const IndentSelector = ({indent, setIndent}) => {
+    
+    return(
+        <div className = 'Selector'>
+            <select value = {indent} onChange = {(e) => setIndent(e.target.value)}>
+                <option value = {2}>2 spaces indent</option>
+                <option value = {4}>4 spaces indent</option>
+            </select>
+            <TriangleDownIcon/>
+        </div>
+    );
+    
+}
+
+export default IndentSelector;
