a03473369b813471d94a5c3892b205226215d0ba
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2021-03-18 10:40:15 +0100

    Download the JSON file

--- src/Result.js
+++ src/Result.js
@@ -1,13 +1,16 @@
-import React, { useState }             from 'react';
-import { ClippyIcon, CheckCircleIcon } from '@primer/octicons-react';
+import React, { useState }                           from 'react';
+import IndentSelector                                from './IndentSelector';
+import { saveAs }                                    from 'file-saver';
+import { ClippyIcon, CheckCircleIcon, DownloadIcon } from '@primer/octicons-react';
 
 const Result = ({json}) => {
     
-    const [alert, setAlert] = useState(null);
+    const [alert, setAlert]   = useState(null);
+    const [indent, setIndent] = useState(2);
     
-    const copyJSON = () => {
+    const copy = () => {
         
-        navigator.clipboard.writeText(JSON.stringify(json, null, 4));
+        navigator.clipboard.writeText(JSON.stringify(json, null, parseInt(indent)));
         
         setAlert('copied');
         
@@ -15,15 +18,34 @@ const Result = ({json}) => {
         
     }
     
+    const download = () => {
+        
+        var fileName = 'result.json';
+        
+        var fileToSave = new Blob([JSON.stringify(json, null, parseInt(indent))], {
+            type: 'application/json',
+            name: fileName
+        });
+        
+        saveAs(fileToSave, fileName);
+        
+        setAlert('downloaded');
+        
+        setTimeout(() => setAlert(null), 1500);
+        
+    }
+    
     return(
         <div className = 'Result'>
-            <textarea value = {JSON.stringify(json, null, 4)} readOnly = {true}/>
-            <button onClick = {copyJSON}>
-                { alert !== 'copied' 
-                ? <><ClippyIcon/>Copy JSON</>
-                : <><CheckCircleIcon/>Copied</>
-                }
-            </button>
+            <IndentSelector
+                indent    = {indent}
+                setIndent = {setIndent}
+            />
+            <textarea value = {JSON.stringify(json, null, parseInt(indent))} readOnly = {true}/>
+            <div className = 'Actions'>
+                <button onClick = {copy}>    { alert !== 'copied'     ? <><ClippyIcon/>Copy JSON</>       : <><CheckCircleIcon/>Copied</>}</button>
+                <button onClick = {download}>{ alert !== 'downloaded' ? <><DownloadIcon/>Download JSON</> : <><CheckCircleIcon/>Downloading</>}</button>
+            </div>
         </div>
     );
     
