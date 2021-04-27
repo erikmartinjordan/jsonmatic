5730130b8960d2e8018b852155f425bac30cd4c8
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2021-04-27 11:23:48 +0200

    Added different routes to app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-import App      from './App';
-import React    from 'react';
-import ReactDOM from 'react-dom';
+import App               from './App';
+import Changelog         from './Changelog';
+import Footer            from './Footer';
+import React             from 'react';
+import ReactDOM          from 'react-dom';
+import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
-ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+            <Route exact path = '/'             component = {App}/>
+            <Route exact path = '/changelog'    component = {Changelog}/>
+        </Switch>
+        <Footer/>
+    </BrowserRouter>
+, document.getElementById('root'));
