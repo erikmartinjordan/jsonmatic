c241cbb1eb7af97557b5c79d20eb29416fb7c08f
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2021-05-03 11:23:51 +0200

    Using UploadFiles forked component

--- src/MultipleReplaces.js
+++ src/MultipleReplaces.js
@@ -1,7 +1,7 @@
-import React, { useState }                               from 'react';
-import { saveAs }                                        from 'file-saver';
-import { DownloadIcon, UploadIcon, TriangleDownIcon }    from '@primer/octicons-react';
-
+import React, { useState }                 from 'react';
+import { saveAs }                          from 'file-saver';
+import UploadFiles                         from './UploadFiles';
+import { DownloadIcon,  TriangleDownIcon } from '@primer/octicons-react';
 
 const MultipleReplaces = () => {
     
@@ -11,33 +11,6 @@ const MultipleReplaces = () => {
     const [path,         setPath]         = useState('');
     const [replaceValue, setReplaceValue] = useState('');
     const [currentValue, setCurrentValue] = useState('');
-    
-    const upload = async (e) => {
-        
-        let files = Array.from(e.target.files).map(file => {
-
-            let reader = new FileReader();
-                
-            return new Promise(resolve => {
-                
-                reader.onload = () => resolve({
-
-                    name: file.name, 
-                    json: JSON.parse(reader.result)
-
-                });
-
-                reader.readAsText(file);
-                
-            });
-            
-        });       
-
-        let jsons = await Promise.all(files);
-        
-        setJsonfiles(jsons);
-        
-    }
     
     const replace = () => {
 
@@ -84,45 +57,34 @@ const MultipleReplaces = () => {
         setNumReplaces(numReplaces);
         
     }
-    
+
     const download = () => {
 
         jsonfiles.forEach(({name, json}) => {
-
+    
             let fileName = name;
-
+    
             let fileToSave = new Blob([JSON.stringify(json, null, 2)], {
                 type: 'application/json',
                 name: fileName
             });
             
             saveAs(fileToSave, fileName);
-
-        });       
-
-    }
     
+        });       
+    
+    }
+
     return(
         <div className = 'Multiple'>
-            { jsonfiles.length === 0
+            <h2>Replace multiple properties in JSON files at once</h2>
+            <p>Select your files (they won't be uploaded to any server), they remain in your browser.</p>
+            <UploadFiles
+                jsonfiles    = {jsonfiles}
+                setJsonfiles = {setJsonfiles}
+            />
+            { jsonfiles.length > 0
             ? <React.Fragment>
-                <h2>Replace multiple properties in JSON files at once</h2>
-                <p>Select your files (they won't be uploaded to any server), they remain in your browser.</p>
-                <div className = 'Upload'>
-                    <UploadIcon/>Click here to upload files
-                    <input onChange = {upload} type = 'file' title = '' multiple/>
-                </div> 
-              </React.Fragment>
-            : <React.Fragment>
-                <h2>Files</h2>
-                <div className = 'Grid'>
-                    {jsonfiles.map(({name, json}, key) => 
-                        <div className = 'Block' key = {key}>
-                            <div className = 'Name'>{name}</div>
-                            {JSON.stringify(json, null, 2)}
-                        </div>)
-                    }
-                </div>
                 <h2>Rules</h2>
                 <div className = 'Replace'>
                     <span>If</span>
@@ -147,6 +109,7 @@ const MultipleReplaces = () => {
                     <div>{numReplaces !== null ? `${numReplaces} fields replaced` : null}</div>                                      
                 </div>
               </React.Fragment>
+            : null
             }
         </div>
     );
