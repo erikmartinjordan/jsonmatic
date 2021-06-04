c9123311dbd16af4d7af8d8f6d19027ad26af3bb
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2021-06-04 10:25:53 +0200

    Revert "Fixed typo"
    This reverts commit d8e909264cc9dc600b43cc74aa39b72ad13a9020.

--- src/TransformToJSON.js
+++ src/TransformToJSON.js
@@ -62,7 +62,7 @@ const TransformToJSON = ({csv, json, setJson}) => {
         
         if(duplicateKeys){
             
-            setJson({'Error': 'JSON has duplicated keys'});
+            setJson({'Error': 'JSON has duplicate keys'});
             
         }
         else if(firstRowEmpty){
